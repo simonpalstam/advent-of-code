122a393f5c515ff76bec961ff4cae9593e3bdb0d
simonpalstam <simon.palstam@wearelevels.com>
2023-12-04 21:48:58 +0100

    2023 day 2

---
+++ src/main/kotlin/se/simon/Template.kt
@@ -0,0 +1,20 @@
+package se.simon
+
+
+fun main() {
+    println("Problem 1 result: ${Day.problem1()}")
+    println("Problem 2 result: ${Day.problem2()}")
+}
+object Day {
+    fun problem1(): Any {
+        return "TODO"
+    }
+
+    fun problem2(): Any {
+        return "TODO"
+    }
+
+    private val data = """
+        
+    """.trimIndent()
+}
